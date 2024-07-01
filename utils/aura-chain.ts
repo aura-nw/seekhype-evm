fe127b9fdc65331dd3cc29327b3229579570de4f
CloudCludfore <CloudCludfore@gmail.com>
2024-07-01 14:51:11 +0700

    commit euphoria

--- utils/aura-chain.ts
+++ utils/aura-chain.ts
@@ -30,38 +30,8 @@ import { defineChain, zeroAddress } from 'viem'
 //   ],
 // })
 
-export const aura = /*#__PURE__*/ defineChain({
-  id: 1236,
-  name: 'Aura EVM',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'aura',
-    symbol: 'aura',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://jsonrpc.serenity.aura.network/'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Aura EVM Block Explorer',
-      url: 'https://serenity.aurascan.io',
-    },
-  },
-  paymentTokens: [
-    {
-      chainId: 1236,
-      address: zeroAddress,
-      symbol: 'aura',
-      name: 'aura',
-      decimals: 18,
-    },
-  ],
-})
-
 // export const aura = /*#__PURE__*/ defineChain({
-//   id: 6321,
+//   id: 1236,
 //   name: 'Aura EVM',
 //   nativeCurrency: {
 //     decimals: 18,
@@ -70,18 +40,18 @@ export const aura = /*#__PURE__*/ defineChain({
 //   },
 //   rpcUrls: {
 //     default: {
-//       http: ['https://jsonrpc.euphoria.aura.network/'],
+//       http: ['https://jsonrpc.serenity.aura.network/'],
 //     },
 //   },
 //   blockExplorers: {
 //     default: {
 //       name: 'Aura EVM Block Explorer',
-//       url: 'https://euphoria.aurascan.io',
+//       url: 'https://serenity.aurascan.io',
 //     },
 //   },
 //   paymentTokens: [
 //     {
-//       chainId: 6321,
+//       chainId: 1236,
 //       address: zeroAddress,
 //       symbol: 'aura',
 //       name: 'aura',
@@ -89,6 +59,36 @@ export const aura = /*#__PURE__*/ defineChain({
 //     },
 //   ],
 // })
+
+export const aura = /*#__PURE__*/ defineChain({
+  id: 6321,
+  name: 'Aura EVM',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'aura',
+    symbol: 'aura',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://jsonrpc.euphoria.aura.network/'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Aura EVM Block Explorer',
+      url: 'https://euphoria.aurascan.io',
+    },
+  },
+  paymentTokens: [
+    {
+      chainId: 6321,
+      address: zeroAddress,
+      symbol: 'aura',
+      name: 'aura',
+      decimals: 18,
+    },
+  ],
+})
 // export const aura = /*#__PURE__*/ defineChain({
 //   id: 6322,
 //   name: 'Aura EVM',
