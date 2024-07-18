f215a908fa90839b0db12c7509bd9f2f392df033
CloudCludfore <CloudCludfore@gmail.com>
2024-07-18 15:02:16 +0700

    udpate

--- utils/aura-chain.ts
+++ utils/aura-chain.ts
@@ -60,37 +60,8 @@ import { defineChain, zeroAddress } from 'viem'
 //   ],
 // })
 
-export const aura = /*#__PURE__*/ defineChain({
-  id: 6321,
-  name: 'Aura EVM',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'aura',
-    symbol: 'aura',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://jsonrpc.euphoria.aura.network/'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Aura EVM Block Explorer',
-      url: 'https://euphoria.aurascan.io',
-    },
-  },
-  paymentTokens: [
-    {
-      chainId: 6321,
-      address: zeroAddress,
-      symbol: 'aura',
-      name: 'aura',
-      decimals: 18,
-    },
-  ],
-})
 // export const aura = /*#__PURE__*/ defineChain({
-//   id: 6322,
+//   id: 6321,
 //   name: 'Aura EVM',
 //   nativeCurrency: {
 //     decimals: 18,
@@ -99,18 +70,18 @@ export const aura = /*#__PURE__*/ defineChain({
 //   },
 //   rpcUrls: {
 //     default: {
-//       http: ['https://jsonrpc.aura.network/'],
+//       http: ['https://jsonrpc.euphoria.aura.network/'],
 //     },
 //   },
 //   blockExplorers: {
 //     default: {
 //       name: 'Aura EVM Block Explorer',
-//       url: 'https://aurascan.io',
+//       url: 'https://euphoria.aurascan.io',
 //     },
 //   },
 //   paymentTokens: [
 //     {
-//       chainId: 6322,
+//       chainId: 6321,
 //       address: zeroAddress,
 //       symbol: 'aura',
 //       name: 'aura',
@@ -118,3 +89,32 @@ export const aura = /*#__PURE__*/ defineChain({
 //     },
 //   ],
 // })
+export const aura = /*#__PURE__*/ defineChain({
+  id: 6322,
+  name: 'Aura EVM',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'aura',
+    symbol: 'aura',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://jsonrpc.aura.network/'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Aura EVM Block Explorer',
+      url: 'https://aurascan.io',
+    },
+  },
+  paymentTokens: [
+    {
+      chainId: 6322,
+      address: zeroAddress,
+      symbol: 'aura',
+      name: 'aura',
+      decimals: 18,
+    },
+  ],
+})
